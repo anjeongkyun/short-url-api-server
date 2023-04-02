bc80db11c11aaa7cbf9ebed2416f1806bb3f0d4f
anjeongkyun <dkswjdrbs12@naver.com>
2023-04-02 23:55:15 +0900

    feat: generate-url의 util 함수들을 구현한다

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
-import validateEnv from "./utils/validateEnv";
-import App from '@/app';
+import validateEnv from "./utils/validate-env";
+import App from "@/app";
 import { HealthRoute } from "@/routes/health.route";
 
 validateEnv();
 
-const app = new App([
-  new HealthRoute()
-]);
+const app = new App([new HealthRoute()]);
 
 app.listen();
-
---
+++ src/utils/generate-url.ts
@@ -0,0 +1,36 @@
+import crypto from "crypto";
+
+const MIN_URL_LENGTH: number = 43;
+const SHORTENED_URL_LENGTH: number = 5;
+const domain: string = "shortener-url";
+
+export const generateShortUrl = (originalUrl: string): string => {
+  const hash: Buffer = crypto.createHash("sha256").update(originalUrl).digest();
+  const shortUrl: string = encodeBase64Url(hash).substring(0, MIN_URL_LENGTH);
+  return (
+    extractProtocol(originalUrl) +
+    domain +
+    "/" +
+    randomSortingUrl(shortUrl, SHORTENED_URL_LENGTH)
+  );
+};
+
+const encodeBase64Url = (buffer: Buffer): string => {
+  let base64 = buffer.toString("base64");
+  base64 = base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
+  return base64;
+};
+
+const randomSortingUrl = (shortUrl: string, length: number): string => {
+  let result = "";
+  Array.from({ length }, () => {
+    result += shortUrl.charAt(Math.floor(Math.random() * shortUrl.length));
+  });
+  return result;
+};
+
+const extractProtocol = (url: string): string => {
+  const regex = /^(https?:\/\/)/;
+  const match = regex.exec(url);
+  return match[1];
+};
