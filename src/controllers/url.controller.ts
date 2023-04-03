bf26ddc76c5196c16d7559633efe5a8bd4557169
anjeongkyun <dkswjdrbs12@naver.com>
2023-04-04 00:01:10 +0900

    feat: url controller를 생성하고 createUrl을 구현한다

---
+++ src/controllers/url.controller.ts
@@ -0,0 +1,28 @@
+import { UsecaseContext } from "@/contexts";
+import { CreateUrlCommand } from "@/contract/create-url.command";
+import { Url } from "@/entities/url";
+import { CreateUrlUsecase } from "@/usecases/create-url.usecase";
+import { NextFunction, Request, Response } from "express";
+
+export class UrlController {
+  private createUrlUsecsae: CreateUrlUsecase;
+
+  constructor(usecaseContext: UsecaseContext) {
+    this.createUrlUsecsae = usecaseContext.createUrlUsecase;
+  }
+
+  createUrl = async (req: Request, res: Response, next: NextFunction) => {
+    const command: CreateUrlCommand = req.body;
+    this.createUrlUsecsae
+      .execute(command)
+      .then((result: Url) => res.status(200).json({ data: result }))
+      .catch((err) => {
+        console.error(err);
+        if (err instanceof Error) {
+          next(new Error("invalid url"));
+          return;
+        }
+        next(err);
+      });
+  };
+}
